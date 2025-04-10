c5b4bde779df475b47f662d0fafd2c2422bf8881
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2025-04-10 16:11:32 +0300

    using-the-slug-and-updating-field-options

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 
 from .models import Book
 
@@ -9,3 +10,17 @@ def index(request):
   return render(request, "book_outlet/index.html", {
     "books": books
   })
+
+def book_detail(request, slug):
+  # try:
+  #   book = Book.objects.get(pk=id)
+  # except:
+  #   raise Http404()
+  book = get_object_or_404(Book, slug=slug)
+  return render(request, "book_outlet/book_detail.html", {
+    "title": book.title,
+    "author": book.author,
+    "rating": book.rating,
+    "is_bestseller": book.is_bestselling
+  }
+)

bd28ff10bf6a63697948d1258c4247b54cd349f3
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2025-04-19 18:25:39 +0300

    deployment-01-serving-static-files-with-django

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7)vxs-5+#(=sf_iby)@zao9o8=&y#n(tv6y4p&-o^629zln)@0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -118,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"

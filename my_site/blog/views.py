6063f0afa66ba28cc936996b4cef6ade58688186
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2025-04-19 17:43:22 +0300

    prj-blog-uploads-sessions-11-finishing-the-read-later-feature

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -9,6 +9,7 @@ from .forms import CommentForm
 
 # Create your views here.
 
+
 class StartingPageView(ListView):
     template_name = "blog/index.html"
     model = Post
@@ -29,14 +30,26 @@ class AllPostsView(ListView):
 
 
 class SinglePostView(View):
+    def is_stored_post(self, request, post_id):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+          is_saved_for_later = post_id in stored_posts
+        else:
+          is_saved_for_later = False
+
+        return is_saved_for_later
+
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
+        
         context = {
-          "post": post,
-          "post_tags": post.tags.all(),
-          "comment_form": CommentForm(),
-          "comments": post.comments.all().order_by("-id")
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
         }
+
         return render(request, "blog/post-detail.html", context)
 
     def post(self, request, slug):
@@ -44,20 +57,23 @@ class SinglePostView(View):
         post = Post.objects.get(slug=slug)
 
         if comment_form.is_valid():
-          comment = comment_form.save(commit=False)
-          comment.post = post
-          comment.save()
-          
-          return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
 
         context = {
-          "post": post,
-          "post_tags": post.tags.all(),
-          "comment_form": comment_form,
-          "comments": post.comments.all().order_by("-id")
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
         }
+
         return render(request, "blog/post-detail.html", context)
-    
+
+
 class ReadLaterView(View):
     def get(self, request):
         stored_posts = request.session.get("stored_posts")
@@ -74,7 +90,6 @@ class ReadLaterView(View):
 
         return render(request, "blog/stored-posts.html", context)
 
-
     def post(self, request):
         stored_posts = request.session.get("stored_posts")
 
@@ -85,6 +100,9 @@ class ReadLaterView(View):
 
         if post_id not in stored_posts:
           stored_posts.append(post_id)
-          request.session["stored_posts"] = stored_posts
+        else:
+          stored_posts.remove(post_id)
+
+        request.session["stored_posts"] = stored_posts
         
         return HttpResponseRedirect("/")

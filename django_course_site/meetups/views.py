29a78f8efdc05c311ad12b76b0d3b5428513604b
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2025-04-21 20:12:53 +0300

    summary-13-creating-a-model-form

--- django_course_site/meetups/views.py
+++ django_course_site/meetups/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import Meetup
+from .forms import RegistrationForm
 
 # Create your views here.
 
@@ -14,10 +15,19 @@ def index(request):
 def meetup_details(request, meetup_slug):
     try:
         selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        if request.method == 'GET':
+            registration_form = RegistrationForm()
+        else:
+            registration_form = RegistrationForm(request.POST)
+            if registration_form.is_valid():
+                participant = registration_form.save()
+                selected_meetup.participants.add(participant)             
+
         return render(request, 'meetups/meetup-details.html', {
-            'meetup_found': True,
-            'meetup': selected_meetup
-        })
+                'meetup_found': True,
+                'meetup': selected_meetup,
+                'form': registration_form
+            })
     except Exception as exc:
         return render(request, 'meetups/meetup-details.html', {
             'meetup_found': False
